93d70d6d95e95debbd179057fef34c31391cccf5
JavaCoffeeBean <JavaCoffeeBean@gmail.com>
2019-10-18 05:58:08 -0500

    added a new layout to test scrolling of the home image along with recyclerview

--- app/src/main/java/com/example/android/munchiez/MainActivity.java
+++ app/src/main/java/com/example/android/munchiez/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_page);
+        setContentView(R.layout.scroll_test);
     }
 }
